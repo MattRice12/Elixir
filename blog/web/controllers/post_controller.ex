a6cf0215e65600403e246a55ab2d7337efc30781
Matt Rice <mattrice12@outlook.com>
2017-05-25 16:21:23 -0500

    added edit/update actions

--- blog/web/controllers/post_controller.ex
+++ blog/web/controllers/post_controller.ex
@@ -4,12 +4,17 @@ defmodule Blog.PostController do
 
   def index(conn, _params) do
     posts = Repo.all(Post)
-    render(conn, "index.html", posts: posts)
+    render(conn, :index, posts: posts)
+  end
+
+  def show(conn, %{"id" => id}) do
+    post = Repo.get!(Post, id)
+    render(conn, :show, post: post)
   end
 
   def new(conn, _params) do
     changeset = Post.changeset(%Post{})
-    render(conn, "new.html", changeset: changeset)
+    render(conn, :new, changeset: changeset)
   end
 
   def create(conn, %{"post" => post_params}) do
@@ -21,15 +26,42 @@ defmodule Blog.PostController do
         |> redirect(to: post_path(conn, :index))
       {:error, changeset} ->
         conn
-        |> put_flash(:info, "Error")
-        |> render("new.html", changeset: changeset)
+        |> put_flash(:info, "Something went wrong!")
+        |> render(:new, changeset: changeset)
     end
   end
+
+  def edit(conn, %{"id" => id}) do
+    post = Repo.get!(Post, id)
+    changeset = Post.changeset(post)
+    render(conn, :edit, post: post, changeset: changeset)
+  end
+
+
+
+  def update(conn, %{"id" => id, "post" => post_params}) do
+    post = Repo.get!(Post, id)
+    changeset = Post.changeset(post, post_params)
+
+    case Repo.update(changeset) do
+      {:ok, post} ->
+        conn
+        |> put_flash(:info, "Post updated successfully!")
+        |> redirect(to: post_path(conn, :show, post))
+      {:error, changeset} ->
+        render(conn, :edit, post: post, changeset: changeset)
+    end
+  end
+
+
+
+
+
 
   def delete(conn, %{"id" => id}) do
     post = Repo.get!(Post, id) |> Repo.delete!
     conn
-    |> put_flash(:info, "Deleted Post!")
+    |> put_flash(:info, "Post Deleted!")
     |> redirect(to: post_path(conn, :index))
   end
 
